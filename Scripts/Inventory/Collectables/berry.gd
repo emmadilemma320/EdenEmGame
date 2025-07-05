789791cad6181420e7e9826ed00cdb65f0255e2d
emmadilemma320 <emma.umiko.pedersen@gmail.com>
2025-07-05 14:36:10 -0700

    Berry pick-up
    Changed the berry item so you pick it up with space, and it has a label that appears when you hover over it

--- Scripts/Inventory/Collectables/berry.gd
+++ Scripts/Inventory/Collectables/berry.gd
@@ -1,8 +1,25 @@
 extends Area2D
 
 @export var inventory_self: InventoryCollectable = preload("res://Resources/Inventory/Collectables/berry.tres")
+@onready var name_label: Label = $name
+@onready var player
+@onready var player_in_area: bool
+
+func _process(delta):
+	if player_in_area and Input.is_action_pressed("item_pick_up"):
+		print("berry nice!")
+		player.collect(inventory_self)
+		queue_free()
+	
+func _on_mouse_entered() -> void:
+	name_label.visible = true
+	
+func _on_mouse_exited() -> void:
+	name_label.visible = false
 
 func _on_body_entered(body: Node2D) -> void:
-	print("berry nice!")
-	body.collect(inventory_self)
-	queue_free()
+	player = body
+	player_in_area = true
+	
+func _on_body_exited(body: Node2D) -> void:
+	player_in_area = false
--- Scripts/Inventory/collectable_item.gd
+++ Scripts/Inventory/collectable_item.gd
@@ -4,6 +4,7 @@ class_name CollectableItem extends Area2D
 @export var collection_message: String
 var collision = CollisionShape2D.new()
 #var sprite = Sprite2D.new()
+var label = Label.new()
 
 func _init() -> void:
 	collision.shape = CircleShape2D.new()
@@ -12,7 +13,18 @@ func _init() -> void:
 	
 	#sprite.texture = inventory_self.texture
 	#add_child(sprite)
+	
+	label.text = "berry"
+	label.z_index = 1
+	label.theme = preload("res://Assets/UI/Theme/in_world_theme.tres")
+	add_child(label)
 
 func _on_body_entered(body: Node2D) -> void:
 	body.collect(inventory_self)
 	queue_free()
+	
+func _on_mouse_entered() -> void:
+	label.visible = true
+	
+func _on_mouse_exited() -> void:
+	label.visible = false
