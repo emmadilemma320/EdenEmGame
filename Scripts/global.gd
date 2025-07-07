c4f6eb50b78215692cea2f1d8592d9585ccdf02c
emmadilemma320 <emma.umiko.pedersen@gmail.com>
2025-07-07 16:42:57 -0700

    bug fix

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -7,9 +7,10 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	discovered.append("amanita")
+	current_open_menu.append("none")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
