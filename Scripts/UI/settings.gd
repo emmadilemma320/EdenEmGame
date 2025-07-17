3cea4857146adc28623c215ea871eb057b70b5ef
emmadilemma320 <emma.umiko.pedersen@gmail.com>
2025-07-17 13:56:23 -0700

    Settings Menu & Credits

--- Scripts/UI/settings.gd
+++ Scripts/UI/settings.gd
@@ -1,16 +1,27 @@
 extends Control
 
+const SETTINGS_MENU_NAMES: Array = ["Visual Settings", "Audio Settings", "Gameplay Settings", "Credits"]
+
+@onready var settings_label: Label = $Base/Label
+@onready var main_settings_menu: VBoxContainer = $Base/main_settings_list
+@onready var menus: Array
+
+@onready var back_button: TextureButton = $Base/back_button
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	menus = $Base/menus.get_children()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("close_current_menu"):
-		if Global.current_open_menu.has(name) and visible:
+		if Global.current_open_menu.back()==name and visible:
 			close()
+		for i in range(menus.size()):
+			if Global.current_open_menu.back()==menus[i].name and menus[i].visible:
+				close_settings_menus()
 	
 func open():
 	visible = true
@@ -18,6 +29,9 @@ func open():
 	
 func close():
 	visible = false
+	for i in range(menus.size()):
+		if menus[i].visible:
+			close_settings_menus()
 	Global.current_open_menu.erase(name)
 
 
@@ -26,3 +40,23 @@ func _on_settings_button_pressed() -> void:
 		close()
 	else:
 		open()
+
+
+func open_settings_menu(i: int):
+	main_settings_menu.visible = false
+	back_button.visible = true
+	menus[i].visible = true
+	settings_label.text = SETTINGS_MENU_NAMES[i]
+	
+	Global.current_open_menu.append(menus[i].name)
+	
+func close_settings_menus():
+	main_settings_menu.visible = true
+	back_button.visible = false
+	for i in range(menus.size()):
+		if menus[i].visible:
+			menus[i].visible = false
+			Global.current_open_menu.erase(menus[i].name)
+	settings_label.text = "Settings"
+	
+	
