5a5e4965c955e0d07264822b0b2959fba3d3f945
Emma Pedersen <emma.umiko.pedersen@gmail.com>
2026-05-13 14:06:12 -0700

    Added export categories to inventory collectable resource

--- Scripts/Resource Classes/inventory_collectable_class.gd
+++ Scripts/Resource Classes/inventory_collectable_class.gd
@@ -3,7 +3,10 @@ extends Resource
 #Resource (in-inventory) version of a collectable
 class_name InventoryCollectable
 
+@export_category("Basic Info")
 @export var name: String = ""
+
+@export_category("Textures")
 @export var texture: Texture2D
 @export var in_world_texture: Texture2D
 
