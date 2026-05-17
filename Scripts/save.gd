0ae2f66179a52ad3abf30c87a25500ee502f0c8e
Emma Pedersen <emma.umiko.pedersen@gmail.com>
2026-05-17 14:13:57 -0700

    Added new_save method to save.gd
    This initilizes the discovered collectables and recipes lists to arrays of 'false' of the right size, as well as the discovered_npcs dictionary to an empty one

--- Scripts/save.gd
+++ Scripts/save.gd
@@ -10,12 +10,19 @@ var discovered_recipes: Array[bool]
 # town progress 
 var discovered_npcs: Dictionary[String, int]
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func new_save() -> void:
+	discovered_collectables.resize(collectables_master_list.size())
+	discovered_collectables.fill(false)
+	
+	discovered_recipes.resize(recipes_master_list.size())
+	discovered_recipes.fill(false)
+	
+	discovered_npcs = {}
