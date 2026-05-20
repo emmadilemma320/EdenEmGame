98ae43e3b2d8835028b91841b9b96ca84df466c5
Emma Pedersen <emma.umiko.pedersen@gmail.com>
2026-05-20 15:04:30 -0700

    Basic function structure
    Created the necessary functions and their parameters (no functionality yet)

--- Scripts/UI/Grimoire/catalogue.gd
+++ Scripts/UI/Grimoire/catalogue.gd
@@ -7,7 +7,25 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	# To start, we make a list of all collectables (not just the discovered ones)
+	# and sort it alphabetically
+	var all_collectables = Save.COLLECTABLE_LIST.keys()
+	all_collectables.sort()
+	
+	# Then we create an entry for each collectable
+	for collectable in all_collectables:
+		create_entry(collectable)
+	
+
+func create_entry(collectable: String) -> void:
+	var curr_index = 0
+	print("Creating entry for ", collectable)
+	
+	if Save.discovered_collectables[curr_index]:
+		reveal(curr_index)
+	
+func reveal(i: int) -> void:
+	print("Entry ", i, " revealed!")
 
 func open():
 	visible = true
