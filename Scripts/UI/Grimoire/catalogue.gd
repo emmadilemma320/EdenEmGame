8e979230e3c36cc1b12f5a66da15b608b97fa2c7
Emma Pedersen <emma.umiko.pedersen@gmail.com>
2026-05-20 15:50:47 -0700

    Resizing catalogue entries
    Changed the size of the catalogue entries, as well as created code to sense if entries extend off the page

--- Scripts/UI/Grimoire/catalogue.gd
+++ Scripts/UI/Grimoire/catalogue.gd
@@ -5,7 +5,8 @@ extends Control
 @onready var blank_entry = preload("res://Scenes/UI/Grimoire/catalogue_entry.tscn")
 @onready var entries = $entries/grid1
 
-const ENTRY_BUFFER = 5
+const ENTRY_BUFFER = -20
+var max_entries_per_page: Vector2 = Vector2(1, 1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,17 +21,22 @@ func _ready() -> void:
 	
 
 func create_entry(collectable: String) -> void:
-	var curr_index = 0
-	print("Creating entry for ", collectable)
+	var curr_index = entries.get_children().size()
+	print("Creating entry for ", collectable, " at index ", curr_index)
 	
 	var new_entry = blank_entry.instantiate()
 	new_entry.inventory_self = load(Save.COLLECTABLE_LIST[collectable])
 	new_entry.update()
 	
 	if entries.get_children().size() == 0:
-		$entries/grid1.add_theme_constant_override("h_separation", int(new_entry.return_size().x) + ENTRY_BUFFER)
-		$entries/grid1.add_theme_constant_override("v_separation", int(new_entry.return_size().y) + ENTRY_BUFFER)
-
+		entries.add_theme_constant_override("h_separation", int(new_entry.return_size().x) + ENTRY_BUFFER)
+		entries.add_theme_constant_override("v_separation", int(new_entry.return_size().y) + ENTRY_BUFFER)
+		max_entries_per_page[0] = int(entries.size.x) / entries.get_theme_constant("h_separation")
+		max_entries_per_page[1] = int(entries.size.y) / entries.get_theme_constant("v_separation")
+		print(max_entries_per_page)
+	if curr_index + 1 > max_entries_per_page.y:
+		print("Collectable ", collectable, " extends off of page!")
+		
 	if Save.discovered_collectables[curr_index]:
 		new_entry.reveal()
 		
