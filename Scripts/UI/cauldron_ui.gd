1e32308408a0f5c08ab6cff99663cd5bbc6d9f8d
Lea Anderson-Young <lea.r.andersonyoung@gmail.com>
2026-05-21 15:44:33 -0700

    Added cauldron button and potion dictionary
    Added 'brew' button to cauldron UI, added potions dictionary with lists of ingredients.

--- Scripts/UI/cauldron_ui.gd
+++ Scripts/UI/cauldron_ui.gd
@@ -1,9 +1,14 @@
 extends Panel
 
+const potions: Dictionary = { # Temporary recipes, can change or delete later
+	"Poison": ["Amanita"],
+	"Energy": ["Apple", "Berry"],
+	"Healing": ["Blue Mushroom"]
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -15,8 +20,9 @@ func _process(_delta: float) -> void:
 
 func open():
 	visible = true
-	pass
 
 func close():
 	visible = false
-	pass
+
+func _on_brew_pressed() -> void:
+	pass # Replace with function body.
